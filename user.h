73bd1ee69f7d640059da95b4256f99edad89e815
mdant1 <31354473+mdant1@users.noreply.github.com>
2018-02-21 11:40:57 -0800

    Update user.h

--- user.h
+++ user.h
@@ -10,9 +10,6 @@ class User
 {
   public:
       User(string a_user, string pass, int id);
-      void get_password();
-      void get_name();
-      void get_id_num();
   private:
       int id_num;
       string password;
