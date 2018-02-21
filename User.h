0de3f3750eeffea07a034eac9a9eb78efa50e786
ramagician <amar.j.rama@gmail.com>
2018-02-21 11:31:18 -0800

    Create User.h

---
+++ User.h
@@ -0,0 +1,16 @@
+#ifndef USERCLASS
+#define USERCLASS
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class User {
+  public:
+    string username;
+    string password;
+    int id_num;
+};
+
+#endif
